dfa0bc519d46f125fa7eb0990ed9d106f96aaffa
DangerMouseB <dangermouseb@forwarding.cc>
2022-04-19 08:24:02 +0100

    correct AGPL license text

--- src/bones/bones/kernel/tests/fred.py
+++ src/bones/bones/kernel/tests/fred.py
@@ -11,7 +11,7 @@
 # bones is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 # of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU Affero General Public License along with Foobar. If not, see
+# You should have received a copy of the GNU Affero General Public License along with coppertop-bones. If not, see
 # <https://www.gnu.org/licenses/>.
 #
 # **********************************************************************************************************************
